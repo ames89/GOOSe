82371f6255951d96dc1fa928b415c91f16d0ff0c
Angel Echavarria <echavarriasam@gmail.com>
2025-05-05 03:05:11 +0000

    refactor

--- compressor/src/main.rs
+++ compressor/src/main.rs
@@ -17,7 +17,7 @@ struct Args {
     salida: String,
 }
 
-fn rle_compress(input: String, output: String) -> std::io::Result<()> {
+fn rle_compress(input: &str, output: &str) -> std::io::Result<()> {
     let input_file = File::open(input)?;
     let mut reader = BufReader::new(input_file);
     let output_file = File::create(output)?;
@@ -26,25 +26,22 @@ fn rle_compress(input: String, output: String) -> std::io::Result<()> {
     reader.read_to_end(&mut buffer)?;
     let encoded = rle::encode(&buffer);
     writer.write_all(&encoded)?;
-
     Ok(())
 }
 
-fn rle_decompress(input: String, output: String) -> std::io::Result<()> {
+fn rle_decompress(input: &str, output: &str) -> std::io::Result<()> {
     let input_file = File::open(input)?;
     let mut reader = BufReader::new(input_file);
     let output_file = File::create(output)?;
     let mut writer = BufWriter::new(output_file);
-
     let mut buffer = Vec::new();
     reader.read_to_end(&mut buffer)?;
     let decoded = rle::decode(&buffer);
     writer.write_all(&decoded)?;
-
     Ok(())
 }
 
-fn rle_compress_decompress(input: String, output: String, compress: bool) -> std::io::Result<()> {
+fn rle_compress_decompress(input: &str, output: &str, compress: bool) -> std::io::Result<()> {
     if compress {
         rle_compress(input, output)
     } else {
@@ -52,22 +49,23 @@ fn rle_compress_decompress(input: String, output: String, compress: bool) -> std
     }
 }
 
-fn file_opener() -> Result<(), std::io::Error> {
+fn file_opener() -> std::io::Result<()> {
     let args = Args::parse();
-    let _input: String = args.entrada;
-    let _output: String = args.salida;
-    println!("Hello, world!");
-    if !PathBuf::from(_input.clone()).exists() {
-        println!("No existe el archivo de entrada");
+    let input = &args.entrada;
+    let output = &args.salida;
+
+    if !PathBuf::from(input).exists() {
+        eprintln!("No existe el archivo de entrada");
         return Ok(());
     }
+
     match args.algoritmo.as_str() {
         "rle" => {
             println!("Usando algoritmo RLE");
-            rle_compress_decompress(_input.clone(), _output.clone(), args.comprimir)?;
+            rle_compress_decompress(input, output, args.comprimir)?;
         }
         _ => {
-            return Ok(());
+            eprintln!("Algoritmo no soportado");
         }
     }
 
